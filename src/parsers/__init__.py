c64eb05ced9ca913641067da78ac090ca87601ae
Erkan ŞAHİN <git.erkansahin@gmail.com>
2026-08-06 15:17:06 +0300

    Update __init__.py

--- src/parsers/__init__.py
+++ src/parsers/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """Log parsers for various formats."""
 
 from src.parsers.nginx import NginxParser, LogEntry
@@ -26,7 +25,6 @@ __all__ = [
     "LogEntry", "NginxParser", "ApacheParser", "SyslogParser",
     "WindowsEventParser", "get_parser", "PARSER_REGISTRY",
 ]
-=======
 """Log Parser modülleri"""
 from .nginx import NginxParser
 from .apache import ApacheParser
@@ -34,4 +32,3 @@ from .syslog import SyslogParser
 from .windows import WindowsEventParser
 
 __all__ = ["NginxParser", "ApacheParser", "SyslogParser", "WindowsEventParser"]
->>>>>>> 68d53f75fadd646719ce98c967c981ab4023b2b0
